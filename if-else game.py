5d65d5fd217e53b1856be7675842a51d2a90f4ce
Karuru Midhun Sai <164469674+midhunsai04@users.noreply.github.com>
2024-08-08 21:15:30 +0530

    Add files via upload

---
+++ if-else game.py
@@ -0,0 +1,17 @@
+print("===Marvel Movie Character Creator===")
+print()
+hangingaround=input("do you like hanging around?: ")
+if hangingaround == "no":
+  print("then you're not spiderman")
+else:
+  print("then you're spiderman")
+gravelly=input("do you have a gravelly voice?: ")
+if gravelly== "no":
+  print("aww, then you're not korg")
+else:
+  print("then you're korg")
+marvellous=input("do you often feel marvelous?: ")
+if marvellous== "yes":
+  print("aha! you're captain marvel! hi!")
+else:
+  print("then you're not captain marvel")
