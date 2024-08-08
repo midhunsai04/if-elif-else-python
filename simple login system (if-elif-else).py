1b260b223c5f1b7be30879f2d95eb434d49c3c27
Karuru Midhun Sai <164469674+midhunsai04@users.noreply.github.com>
2024-08-08 21:42:46 +0530

    Add files via upload

---
+++ simple login system (if-elif-else).py
@@ -0,0 +1,14 @@
+print("LOGIN SYSTEM")
+print("++++++++++++")
+username=input("USERNAME>")
+password=input("PASSWORD>")
+if username=="midhun" and password=="midhun11":
+  print("""
+Hey midhun,
+how are you doing.
+i am glad you came back.
+""")
+elif username=="siri" and password=="siri11":
+  print("hey siri, how are you doing")
+else:
+  print("go away")
